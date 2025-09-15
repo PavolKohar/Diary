d830a68fb38a287b683512187ec4ea7b5e53036a
PavolKohar <koharpavol98@gmail.com>
2025-09-15 10:50:17 +0200

    Created basic structure for project

---
+++ src/main/java/com/palci/DiaryApplication.java
@@ -0,0 +1,14 @@
+package com.palci;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class DiaryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryApplication.class,args);
+    }
+}
