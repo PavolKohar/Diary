d07d34d2d5b426204b34e8dc12e56dd503095de2
PavolKohar <koharpavol98@gmail.com>
2025-09-15 19:53:04 +0200

    Fixed bug if you load dashboard with no entry

--- src/main/java/com/palci/DiaryApp/Controllers/ArticleController.java
+++ src/main/java/com/palci/DiaryApp/Controllers/ArticleController.java
@@ -5,6 +5,7 @@ import com.palci.DiaryApp.Models.DTO.ArticleDTO;
 import com.palci.DiaryApp.Models.Services.ArticleService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.SpelEvaluationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,38 +21,51 @@ public class ArticleController {
     ArticleService articleService;
 
     @GetMapping
-    public String renderDashBoard(Model model){
-        ArticleDTO lastEntry = articleService.findLastEntry();
-        model.addAttribute("lastEntry",lastEntry);
-        List<ArticleDTO> topEntries = articleService.getTopArticles();
-        model.addAttribute("topEntries",topEntries);
-        List<ArticleDTO> allEntries = articleService.getAllArticles();
-        model.addAttribute("allEntries",allEntries);
-        List<ArticleDTO> topMoodEntries = articleService.getTopMoodEntries();
-        model.addAttribute("moodTop",topMoodEntries);
-        return "pages/article/dashboard";
+    public String renderDashBoard(Model model,RedirectAttributes redirectAttributes){
+        try {
+            ArticleDTO lastEntry = articleService.findLastEntry();
+            model.addAttribute("lastEntry",lastEntry);
+            List<ArticleDTO> topEntries = articleService.getTopArticles();
+            model.addAttribute("topEntries",topEntries);
+            List<ArticleDTO> allEntries = articleService.getAllArticles();
+            model.addAttribute("allEntries",allEntries);
+            List<ArticleDTO> topMoodEntries = articleService.getTopMoodEntries();
+            model.addAttribute("moodTop",topMoodEntries);
+            return "pages/article/dashboard";
+        }catch (SpelEvaluationException e ){
+            redirectAttributes.addFlashAttribute("error","Not record yet");
+            return "redirect:/";
+
+        }
     }
 
     @GetMapping("/{filter}")
-    public String renderDashBoardWithFilterMood(@PathVariable String filter, Model model){
-        ArticleDTO lastEntry = articleService.findLastEntry();
-        model.addAttribute("lastEntry",lastEntry);
-        List<ArticleDTO> topEntries = articleService.getTopArticles();
-        model.addAttribute("topEntries",topEntries);
-        List<ArticleDTO> allEntries = articleService.getAllArticles();
-        model.addAttribute("allEntries",allEntries);
+    public String renderDashBoardWithFilterMood(@PathVariable String filter, Model model,RedirectAttributes redirectAttributes){
+        try {
+            ArticleDTO lastEntry = articleService.findLastEntry();
+            model.addAttribute("lastEntry",lastEntry);
+            List<ArticleDTO> topEntries = articleService.getTopArticles();
+            model.addAttribute("topEntries",topEntries);
+            List<ArticleDTO> allEntries = articleService.getAllArticles();
+            model.addAttribute("allEntries",allEntries);
 
-        List<ArticleDTO> byMood;
-        switch (filter){
-            case "top-mood" -> byMood = articleService.getTopMoodEntries();
-            case "mid-mood" -> byMood = articleService.getMidMoodEntries();
-            case "low-mood" -> byMood = articleService.getLowMoodEntries();
-            default -> byMood = articleService.getAllArticles();
+            List<ArticleDTO> byMood;
+            switch (filter){
+                case "top-mood" -> byMood = articleService.getTopMoodEntries();
+                case "mid-mood" -> byMood = articleService.getMidMoodEntries();
+                case "low-mood" -> byMood = articleService.getLowMoodEntries();
+                default -> byMood = articleService.getAllArticles();
+            }
+
+            model.addAttribute("moodTop",byMood);
+
+            return "pages/article/dashboard";
+
+        }catch (SpelEvaluationException e){
+            redirectAttributes.addFlashAttribute("error","Not record yet");
+            return "redirect:/";
         }
 
-        model.addAttribute("moodTop",byMood);
-
-        return "pages/article/dashboard";
     }
 
 
