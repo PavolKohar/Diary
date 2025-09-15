b36b808ebaf5a7ce58553b00a2d9cedb3c99789e
PavolKohar <koharpavol98@gmail.com>
2025-09-15 12:26:46 +0200

    Added DTO to form.html and created basics for post mapping

--- src/main/java/com/palci/DiaryApp/Controllers/ArticleController.java
+++ src/main/java/com/palci/DiaryApp/Controllers/ArticleController.java
@@ -2,10 +2,14 @@ package com.palci.DiaryApp.Controllers;
 
 
 import com.palci.DiaryApp.Models.DTO.ArticleDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/article")
@@ -15,6 +19,19 @@ public class ArticleController {
     @GetMapping("/create")
     public String renderCreateForm(@ModelAttribute ArticleDTO articleDTO){
         return "pages/article/form";
+    }
+
+    @PostMapping("/create")
+    public String createEntry(@Valid @ModelAttribute ArticleDTO articleDTO, BindingResult result, RedirectAttributes redirectAttributes){
+        if (result.hasErrors()){
+            return renderCreateForm(articleDTO);
+        }
+
+        // TODO create method to create article
+
+        return "pages/article/dashboard"; // TODO create dashboard.html
+
+
     }
 
 
