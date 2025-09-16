f8786f2ca9b40b4ed96be38b71d2dafd232bb140
PavolKohar <koharpavol98@gmail.com>
2025-09-16 09:52:02 +0200

    Added Methods findByEmail and existsByEmail to User repository

--- src/main/java/com/palci/DiaryApp/data/Repositories/UserRepository.java
+++ src/main/java/com/palci/DiaryApp/data/Repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.palci.DiaryApp.data.Repositories;
 import com.palci.DiaryApp.data.Entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
+    Optional<UserEntity> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
